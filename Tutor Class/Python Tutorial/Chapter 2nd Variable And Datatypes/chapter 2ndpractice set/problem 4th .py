3e046fe4c775c8cd286058ecd1760e90db4731c4
Raman Thakur <ramanit12025@gmail.com>
2025-08-17 20:45:50 +0530

    add new file chapter 2nd

---
+++ Tutor Class/Python Tutorial/Chapter 2nd Variable And Datatypes/chapter 2ndpractice set/problem 4th .py
@@ -0,0 +1,9 @@
+# create a python program to find that given veriable grater that or not
+a=5
+b=4
+print(a>b)
+print(a<b)
+c=int(input("enter 1 st no. :"))
+d=int(input("enter 2nd no. :"))
+print(c>d)
+print(c<d)
---
+++ Tutor Class/Python Tutorial/Chapter 2nd Variable And Datatypes/chapter 2ndpractice set/problem 6th.py
@@ -0,0 +1,5 @@
+# create a program to find the squre the given no. by the User
+a=int(input("enter 1 st no. :"))
+print("Squre",a*a)
+print("squre",a**2)
+# we not write like that (print("squre",a^2)) this is wrorng way in python
