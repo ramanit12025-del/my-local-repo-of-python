010bc690b892ddc429ad534eddc2e35d975f4cab
Raman Thakur <ramanit12025@gmail.com>
2025-08-19 09:41:05 +0530

    it is not compleat

---
+++ Tutor Class/Python Tutorial/Chapter 4th list data type/02-list slicing.py
@@ -0,0 +1,4 @@
+garden=["apple","banana",508,"raman","aman",5.7,True,False]
+garden[3]="Raman"
+print(garden)
+print(garden[1:4])
---
+++ Tutor Class/Python Tutorial/Chapter 4th list data type/tuples/01-tuple method.py
@@ -0,0 +1,19 @@
+# these are opration in tuple Method
+a=(1,2,343,46,4,767,5,43,5,685,"raman","aman","sadhna","mangal","vimal","krishna",False,True,5.79,94.75)
+print(type(a)) # type is a function
+print(a)
+# its 1nd opration of tuple 
+print(a.index(343))
+# its 2rd opration
+repeated=a 
+print(repeated)
+# its 3th opration 
+print(4 in a)
+print(8778 in a)
+# its 4h opration 
+print(len(a))
+e=(1,2,3) 
+a,b,c=(e)
+print(e)
+
+
